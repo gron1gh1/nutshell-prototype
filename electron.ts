63a120b527e90b82d68e04ee201777108c62ebf5
gron1gh1 <gron1gh1@gmail.com>
2021-04-28 18:40:53 +0900

    Add: Login Form Design

--- electron.ts
+++ electron.ts
@@ -7,8 +7,10 @@ let main_window: BrowserWindow
 
 function create_window() {
   main_window = new BrowserWindow({
+    width: 800,
+    height: 600,
     // 이것들은 제가 사용하는 설정이니 각자 알아서 설정 하십시오.
-    alwaysOnTop: true,
+    alwaysOnTop: false,
     center: true,
     fullscreen: false,
     kiosk: !isDev,
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import { Button, Form, Input } from 'antd';
+
+const LoginBG = styled.div`
+  background: #ffbd59;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Logo = styled.img`
+  width: 70%;
+`;
+
+const StyleForm = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 50px;
+`
+function LoginForm() {
+  return (
+    <StyleForm>
+      <Input placeholder="Username" />
+      <Input.Password placeholder="Password" />
+      <Button type="primary" htmlType="submit" style={{ marginTop:'10px',width: '300px' }}>
+        Login & Sign Up
+      </Button>
+    </StyleForm>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <LoginBG>
+      <Logo src={'login_background.png'} alt="logo" />
+      <LoginForm />
+    </LoginBG>
   );
 }
 
